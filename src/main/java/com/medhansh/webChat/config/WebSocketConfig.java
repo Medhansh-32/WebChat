aabe6bf1af8c94b80f15452a7991a2eb8ac4b006
Medhansh Sharma <sharmamedhansh21@gmail.com>
2024-12-07 22:59:53 +0530

    Production Proctocol Initiated

--- src/main/java/com/medhansh/webChat/config/WebSocketConfig.java
+++ src/main/java/com/medhansh/webChat/config/WebSocketConfig.java
@@ -3,11 +3,9 @@ package com.medhansh.webChat.config;
 import com.medhansh.webChat.security.AuthenticationHandshakeInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.util.UriComponents;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -21,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat").setAllowedOrigins(ServletUriComponentsBuilder.fromCurrentContextPath().toUriString()).withSockJS()
+        registry.addEndpoint("/chat").setAllowedOrigins("https://webchatprod.onrender.com").withSockJS()
                 .setInterceptors(new AuthenticationHandshakeInterceptor());
 
     }
