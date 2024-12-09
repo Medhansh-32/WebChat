de309fb8a6cbcc1e54dfa399b265133b5c297cae
Medhansh Sharma <sharmamedhansh21@gmail.com>
2024-12-09 23:54:24 +0530

    OTP , Email Verification Added and new Client Id Issued fix

--- src/main/java/com/medhansh/webChat/config/WebSocketConfig.java
+++ src/main/java/com/medhansh/webChat/config/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat").setAllowedOrigins("https://webchatprod.onrender.com").withSockJS()
+        registry.addEndpoint("/chat").setAllowedOrigins("https://webchatapp-cfhr.onrender.com").withSockJS()
                 .setInterceptors(new AuthenticationHandshakeInterceptor());
 
     }
